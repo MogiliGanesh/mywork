f52a80c0515c93cc7cad73aecf2ffb2e03c97904
AUT <mogiliganesh.m@gmail.com>
2020-04-25 12:01:20 +0530

    subString added

--- Basic-Core_Java/src/strings/String_SubString.java
+++ Basic-Core_Java/src/strings/String_SubString.java
@@ -6,5 +6,10 @@ public class String_SubString {
 		String s="GaneshMogili";  
 		   System.out.println(s.substring(6)); 
 		   System.out.println(s.substring(0,6));
+		   
+		   
+		   System.out.println(java.time.LocalDate.now());  
+		   
+		   System.out.println(java.time.LocalDateTime.now());  
 	}
 }
