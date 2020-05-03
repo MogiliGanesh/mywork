cf115da344f4a5344110654d0559f9044ac75a45
AUT <mogiliganesh.m@gmail.com>
2020-05-03 10:53:01 +0530

    Added Threads

---
+++ Basic-Core_Java/src/java_Thread/Multi_Thread.java
@@ -0,0 +1,31 @@
+package java_Thread;
+
+public class Multi_Thread extends Thread {
+
+	public void run() {
+		System.out.println("java run method");
+		
+		for (int i=0;i<5;i++) {
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) throws InterruptedException {
+
+		Multi_Thread mt1=new Multi_Thread();
+		Multi_Thread mt2=new Multi_Thread();
+		Multi_Thread mt3=new Multi_Thread();
+		mt1.start();
+		mt1.join();   // waits till thread dies
+		System.out.println(mt1.getName());
+		mt3.start();
+		System.out.println(mt1.getName());
+		mt2.start();
+		System.out.println(mt1.getName());
+		
+		
+	}
+
+}
+
+
+
---
+++ Basic-Core_Java/src/strings_regex/Regex.java
@@ -0,0 +1,11 @@
+package strings_regex;
+
+public class Regex {
+
+	public static void main(String[] args) {
+String name="Ganesh Mogili 12";
+
+
+	}
+
+}
