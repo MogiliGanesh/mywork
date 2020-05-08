db999db5533c17cb0b77c113cabe4854f6790a0f
AUT <mogiliganesh.m@gmail.com>
2020-05-08 12:25:34 +0530

    UpDated Rest Web Services

--- Rest_WebServices/src/test/java/com/qa/test/Get_Api_Test.java
+++ Rest_WebServices/src/test/java/com/qa/test/Get_Api_Test.java
@@ -30,6 +30,7 @@ public class Get_Api_Test extends TestBase {
 	String serviceUrl;
 	String apiUrl;
 	String URL;
+	
 
 	@BeforeMethod
 	public void setUp() throws IOException {
@@ -46,47 +47,7 @@ public class Get_Api_Test extends TestBase {
 
 		Rest_Client restClient = new Rest_Client();
 		restClient.get(URL);
-		/*
-		 * // Getting status code int statusCode =
-		 * httpResponse.getStatusLine().getStatusCode();
-		 * System.out.println("Status Code  >>>>" + statusCode);
-		 * Assert.assertEquals(statusCode,
-		 * Expected_Status_Code_200,"status code not matched");
-		 * 
-		 * // string the response as string object String responseString =
-		 * EntityUtils.toString(httpresponce.getEntity(), "UTF-8");
-		 * 
-		 * // converting string to json JSONObject responsejson = new
-		 * JSONObject(responseString); System.out.println("resopnse jon >>>" +
-		 * responsejson);
-		 * 
-		 * //single value assertion: //per_page: String perPageValue =
-		 * TestUtils.getValueByJPath(responsejson, "/per_page");
-		 * System.out.println("value of per page is-->"+ perPageValue);
-		 * Assert.assertEquals(Integer.parseInt(perPageValue), 3);
-		 * 
-		 * //total: String totalValue = TestUtils.getValueByJPath(responsejson,
-		 * "/total"); System.out.println("value of total is-->"+ totalValue);
-		 * Assert.assertEquals(Integer.parseInt(totalValue), 12);
-		 * 
-		 * //get the value from JSON ARRAY: String lastName =
-		 * TestUtils.getValueByJPath(responsejson, "/data[0]/last_name"); String id =
-		 * TestUtils.getValueByJPath(responsejson, "/data[0]/id"); String avatar =
-		 * TestUtils.getValueByJPath(responsejson, "/data[0]/avatar"); String firstName
-		 * = TestUtils.getValueByJPath(responsejson, "/data[0]/first_name");
-		 * 
-		 * System.out.println(lastName); System.out.println(id);
-		 * System.out.println(avatar); System.out.println(firstName); // getting Headers
-		 * from respnose string Header[] headerArray = httpresponce.getAllHeaders();
-		 * 
-		 * // converting array to Hashmap HashMap<String, String> allHeaders = new
-		 * HashMap<String, String>();
-		 * 
-		 * for (Header header : headerArray) { allHeaders.put(header.getName(),
-		 * header.getValue()); } System.out.println("headerArray>>  " + allHeaders);
-		 * 
-		 * 
-		 */
+		
 	}
 
 }
